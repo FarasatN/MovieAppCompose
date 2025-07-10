47a71f451438c9f1598ecce3f26994f72a3b2e00
FarasatN <ferasetnovruzov1@gmail.com>
2025-07-10 14:59:33 +0400

    Hilt dependency added

---
+++ app/src/main/java/com/farasatnovruzov/movieappcompose/TriviaApplication.kt
@@ -0,0 +1,11 @@
+//package com.farasatnovruzov.movieappcompose
+//
+//import android.app.Application
+//import dagger.hilt.android.HiltAndroidApp
+//
+//
+//@HiltAndroidApp
+//class TriviaApplication: Application() {
+//
+//
+//}
